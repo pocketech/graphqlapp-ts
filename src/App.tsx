3f3e4d2f98869cf7c090bc67bd69ff35c21c04c9
Kento Nakano <hakuyowasedatech@gmail.com>
2020-12-09 12:58:25 +0900

    garbagecollection

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-yellow-300">
-          Edit <code>src/App.tsx</code> and save to reload.
+const App = () =>
+  <div>
+    <header>
+      <p className="text-yellow-300">
+        Edit
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    </header>
+  </div>
+
 
 export default App;
