8d1f785a9b56271283a73cdca3ec667531dbe55e
Kento Nakano <hakuyowasedatech@gmail.com>
2020-12-10 10:45:34 +0900

    gqlreqest

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import { ApolloProvider } from '@apollo/client'
+import { ApolloProvider, useQuery, gql } from '@apollo/client'
 import { client } from './index'
+
+const ME = gql`
+  query me { 
+    user(login: "iteachonudemy") {
+      name
+      avatarUrl
+    }
+  }
+`
+const MeData = () => {
+  const { loading, error, data } = useQuery(ME)
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error</p>
+  return <p>{data.user.name}</p>
+}
 
 const App = () =>
   <ApolloProvider client={client}>
     <div>
       <header>
         <p className="text-yellow-300">
-          Edit
-          </p>
+          Hello,GraphQL
+        </p>
+        <MeData />
       </header>
     </div>
   </ApolloProvider>
+
 
 
 export default App
